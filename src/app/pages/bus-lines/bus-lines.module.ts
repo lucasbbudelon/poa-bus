7f08a01cf050b140442529c091aed3ad149aa60d
Lucas B Budelon <lucasbbudelon@gmail.com>
2020-07-31 17:08:07 -0300

    Adicionando bootstrap

---
+++ src/app/pages/bus-lines/bus-lines.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BusLinesComponent } from './bus-lines.component';
+
+
+
+@NgModule({
+  declarations: [BusLinesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class BusLinesModule { }
